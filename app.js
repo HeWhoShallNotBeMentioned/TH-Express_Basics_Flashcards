c83259f148591852fdb4a13b0a0b33a60d17d4e3
Chris Underwood <cunderwoodmn@gmail.com>
2017-10-24 20:56:21 -0700

    add first simple route and nodemon

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response)=> {
+  response.send('My first Express application! With nodemon');
+});
+
+app.listen(3001);
