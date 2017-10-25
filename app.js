8b5e424e203f88374668961825936290ae6f0214
Chris Underwood <cunderwoodmn@gmail.com>
2017-10-24 21:20:35 -0700

    add a second route and some styling

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response)=> {
-  response.send('My first Express application! With nodemon');
+app.get('/', (req, res)=> {
+  res.send('<h1>My first Express application! With nodemon!</h1>');
 });
 
-app.listen(3001);
+app.get('/hello', (req, res)=> {
+  res.send('<h1>Welcome, Javascript Developers</h1>');
+});
+
+app.listen(3001, () => {
+  console.log("The application is running on port 3001.");
+});
