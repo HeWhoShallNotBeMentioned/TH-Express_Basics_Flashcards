2a0884d16243e6d227cb2921fa4793497f993212
Chris Underwood <cunderwoodmn@gmail.com>
2017-10-25 21:15:28 -0700

    added dynamic functionality to card route and created sandbox to have a play area

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 
 const app = express();
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple'
+];
+
+const people = [
+  {first: "a", last: "a" },
+  {first: "b", last: "b" },
+  {first: "c", last: "c" },
+  {first: "d", last: "d"},
+  {first: "e", last: "e"}
+];
 
 app.set('view engine', 'pug');
 
@@ -9,7 +25,11 @@ app.get('/', (req, res)=> {
 });
 
 app.get('/cards', (req, res)=> {
-  res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
+  res.render('card', {prompt: "Who is buried in Grant's tomb?", colors});
+});
+
+app.get('/sandbox', (req, res)=> {
+  res.render('sandbox', { colors, people});
 });
 
 app.listen(3001, () => {
