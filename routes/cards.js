72a66b1442b15e5432c245684cd53f23d639d31b
Chris Underwood <cunderwoodmn@gmail.com>
2017-10-30 18:39:11 -0700

    add random routes for cards, button to switch between question and answer,  ability to enter just a route number, and moved goodbye & user name to the layout file so they are available everywhere in the app

--- routes/cards.js
+++ routes/cards.js
@@ -2,17 +2,34 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data/flashCardData.json').data;
 const cards = data.cards;
+
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashCardId = Math.floor( Math.random() * numberOfCards);
+  res.redirect( `/cards/${flashCardId}?side=question`);
+});
+
 router.get('/:id', (req, res)=> {
   const {side} = req.query;
   const { id } = req.params;
+
+  if (!side) {
+    res.redirect(`/cards/${id}?side=question`);
+  }
+  const name = req.cookies.username;
   const text = cards[id][side];
   console.log(side);
   let {hint} = cards[id];
 
-  const templateData = { text };
+  const templateData = { id, text, name };
 
-  if (side == "question") {
+  if (side === "question") {
     templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = "Answer";
+  } else if ( side === "answer") {
+      templateData.sideToShow = 'question';
+      templateData.sideToShowDisplay = "Question";
   }
   res.render('card', templateData);
 });
